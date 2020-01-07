4bee39833c8ac3284889ef8b3997cc69bd50790d
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-07 17:45:45 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -25,8 +25,8 @@ class Waiter
     end
   end
  
-  def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
+  def new_meal(customer, total, tip=0)
+    Meal.new(customer, self, total, tip)
   end
 end 
   

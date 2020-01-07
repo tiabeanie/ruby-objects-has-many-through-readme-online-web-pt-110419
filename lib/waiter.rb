7c636a2b0afa303ca63469ffa59279972a539848
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-07 17:40:47 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
-class Waiter
-
-end
+class Waiter 
+  attr_accessor :name, :yoe
+ 
+  @@all = []
+ 
+  def initialize(name, yoe)
+    @name = name
+    @yoe = yoe
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ end 

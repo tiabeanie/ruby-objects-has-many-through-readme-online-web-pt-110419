3b7a61369bcea7e9954ce67b2fd99a1b4ae92793
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-07 17:52:10 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,7 +20,8 @@ class Waiter
   end
  
   def best_tipper
-    
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
   end 
  
   def new_meal(customer, total, tip=0)
